982abb84a420711d934f1375b6860b11fc6e3249
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-11-21 01:05:22 +0000

    Finished Project

--- image_edit.py
+++ image_edit.py
@@ -4,3 +4,16 @@ import os
 path ='./imgs'
 pathOut = '/editedImgs'
 
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+    
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    
+    clean_name = os.path.splitext(filename)[0]
+    
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
+    
